a80a70d753cc98028fa87c98cb3bab4860c4caa2
vasuquo <veasuquo@gmail.com>
2025-11-06 08:39:14 +0100

    Update change

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,42 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+
+    constructor(productId, dataSource){
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        document
+        .getElementById('addToCart')
+        .addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    addProductToCart() {
+        const cartItems = getLocalStorage("so-cart");
+        cartItems.push(this.product);
+        setLocalStorage("so-cart", cartItems);
+    }
+        
+    renderProductDetails() {
+        const container = document.querySelector(".container");
+        const template = document.querySelector("#product-template");
+        const clone = template.content.cloneNode(true);
+        const [brandName, name, img, price, color, descrp, cartBtn] = clone.querySelectorAll("h3, h2, img, p, p, p, button");        
+        brandName.textContent = this.product.Brand.Name;
+        name.textContent = this.product.NameWithoutBrand;
+        img.src = this.product.Image;
+        img.alt = this.product.NameWithoutBrand;
+        price.textContent = `$${this.product.ListPrice}`;
+        color.textContent = this.product.Colors[0].ColorName;
+        descrp.innerHTML = this.product.DescriptionHtmlSimple;
+        cartBtn.textContent = "Add to Cart";
+        cartBtn.dataset.id =  this.product.Id;
+
+        container.appendChild(clone);
+    }
+}
