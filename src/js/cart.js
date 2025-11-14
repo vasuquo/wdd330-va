f36e701b487ab52d8e9e2d79b4f4932c66c0663e
vasuquo <veasuquo@gmail.com>
2025-11-14 11:44:35 +0100

    template changes 2

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,7 @@ import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 const cartItems = getLocalStorage("so-cart");
-const cartElement = document.querySelector(".product-list")
+const cartElement = document.querySelector(".product-list");
 
 const shoppingCart = new ShoppingCart(cartItems, cartElement);
 
