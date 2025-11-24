927fe0ff5d7f15d073636bdc9bc61cd37e8b5cfd
vasuquo <veasuquo@gmail.com>
2025-11-24 23:39:14 +0100

    Checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,2 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
+
+const orderElement = document.querySelector(".order-summary");
+const zipcode = document.getElementById("zip");
+const checkoutProcess = new CheckoutProcess("so-cart", orderElement);
 loadHeaderFooter();
+checkoutProcess.init();
+zipcode.addEventListener("change", () => {
+    checkoutProcess.calculateOrderTotal();
+});
+
